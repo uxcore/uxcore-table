85582e4db8f9c8d125e7753202782e2b421b36d8
光弘 <guanghong.wsj@alibaba-inc.com>
2015-11-19 19:43:46 +0800

    fix issue #70

--- src/Cell/Util.js
+++ src/Cell/Util.js
@@ -1,6 +1,10 @@
 var util={
     formatValue: (value, type, delimiter) => {
         delimiter = delimiter || " ";
+        if (value === null || value === undefined) {
+            return value;
+        }
+        value = value + "";
         if (type == "money") {
             return value.replace(/(\d{3})(?![$|\.|\(])/g, "$1" + delimiter);
         }
