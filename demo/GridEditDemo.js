702aace0d2dae5d64d8feeb302fce926613d17ac
eternalsky <wsj7552715@hotmail.com>
2016-05-11 20:01:19 +0800

    fix issue #128

--- demo/GridEditDemo.js
+++ demo/GridEditDemo.js
@@ -93,7 +93,7 @@ class Demo extends React.Component {
             { dataKey: 'jsxid', title: 'jsxid', width: 80},
             { dataKey: 'city', editKey: 'cityId',title:'城市', width: 200, type:'select', children: [{id: 'bj', name: '北京'},{id: 'hz', name: '杭州'}].map((item) => {
                 return <Option key={item.id}>{item.name}</Option>
-            }), config: {filterOption: false}, canEdit: function(rowData) { return rowData.name !== "小王"}},
+            }), config: {filterOption: false}, canEdit: function(rowData) { return rowData.name !== "小王"}, rules: {validator: function() {return false}, errMsg: '出错了'}},
             // { dataKey: 'place', title: '城市区间', width: 500, type: 'custom', customField: PlaceSelect},
             { dataKey: 'name', editKey: 'nameId', title:"姓名", width: 200, type:"custom", customField: RadioField, children: [{id: 'xiaoli', name: '小李'}, {id: 'xiaowang', name: '小王'}].map((item) => {
                 return <RadioItem key={item.id} text={item.name} value={item.id} />
