0b5009429b2a54310f1b9c1aa6ccd9208b61a903
光弘 <guanghong.wsj@alibaba-inc.com>
2017-08-07 15:06:37 +0800

    ver. 7.1.0

--- demo/index.jsx
+++ demo/index.jsx
@@ -15,8 +15,8 @@ const Demo3 = require('./TreeGridDemo');
 const Demo4 = require('./GridViewModeDemo');
 const Demo5 = require('./TableGroup');
 
-// ReactDOM.render(<Demo />, document.getElementById('UXCoreDemo'));
-// ReactDOM.render(<Demo2 />, document.getElementById('UXCoreDemo2'));
+ReactDOM.render(<Demo />, document.getElementById('UXCoreDemo'));
+ReactDOM.render(<Demo2 />, document.getElementById('UXCoreDemo2'));
 // ReactDOM.render(<Demo3 />, document.getElementById('UXCoreDemo3'));
 // ReactDOM.render(<Demo4 />, document.getElementById('UXCoreDemo4'));
 ReactDOM.render(<Demo5 />, document.getElementById('UXCoreDemo5'));
