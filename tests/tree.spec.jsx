f691796fa6ccff14949a16a80565e79c26ef3ac0
qianchuan.hw <qianchuan.hw@alibaba-inc.com>
2018-06-12 17:14:39 +0800

    add lie module to fit Promise

--- tests/tree.spec.jsx
+++ tests/tree.spec.jsx
@@ -2,10 +2,10 @@ import expect from 'expect.js';
 import React from 'react';
 import Enzyme, { mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-15';
+import Promise from 'lie';
 // import sinon from 'sinon';
 
 import Table from '../src';
-import { promise } from 'when';
 
 Enzyme.configure({ adapter: new Adapter() });
 
